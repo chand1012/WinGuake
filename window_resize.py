5b4783230b0b9588a1d9f2a94a5b98d7de4b6e35
chand1012 <chandlerl14@outlook.com>
2017-01-18 17:24:40 -0500

    Added color option

--- window_resize.py
+++ window_resize.py
@@ -1,23 +1,7 @@
 import win32api, win32gui
 import os, sys
+from configparser import *
 
-
-#old code
-'''
-def quickresize(columns, lines):c #this may be reused
-    os.system("mode con: cols={} lines={}".format(columns, lines))
-
-# try to use the win32 api to grab and move the windows
-def move_window(window, x, y, wx, wy):
-    win32gui.MoveWindow(window, x, y, wx, wy, True)
-
-def console():
-    os.system('start')
-    return win32.GetConsoleWindow()
-'''
-
-#this is perfect for my PC
-#quickresize(192, 25)
 
 def enumHandler(hwnd, lParam):
     if win32gui.IsWindowVisible(hwnd):
@@ -28,4 +12,14 @@ def enumHandler(hwnd, lParam):
             w_length = int(m_length/2)
             win32gui.MoveWindow(hwnd, 0, 0, w_width, w_length, True)
 
+def main(home, color):
+    '''
+    if home is '' or home is None:
+        home = os.getenv("USERPROFILE")
+    os.system('c:')
+    os.system("cd {}".format(home))
+    '''
+    os.system('color {}'.format(color))
+
 win32gui.EnumWindows(enumHandler, None)
+main(None, '0b')
