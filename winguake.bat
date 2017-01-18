599cfa3cb2fba4ca443e95de7737d269f4a077d7
chand1012 <chandlerl14@outlook.com>
2017-01-18 17:26:45 -0500

    needs fixed, but has been updated

--- winguake.bat
+++ winguake.bat
@@ -1,3 +1,2 @@
 @echo off
-if /I %1=='-settings' python %~dp0/edit_settings.py else cmd /k %~dp0/start_winguake.bat
-start "" "Console.ahk"
+if /I %1=='-settings' python %~dp0/edit_settings.py else cmd /k %~dp0/start_winguake.bat && start "" "Console.ahk"
