6bb245f01a559d3b5c0dc753240bac39019a8e09
chand1012 <chandlerl14@outlook.com>
2017-02-04 23:28:10 -0500

    Added error handling, intergrated window_resize.py

--- winguake_allinone.py
+++ winguake_allinone.py
@@ -1,6 +1,20 @@
 import os, sys
 import argparse
 import subprocess
+import win32api, win32gui
+
+def enumHandler(hwnd, lParam):
+    if win32gui.IsWindowVisible(hwnd):
+        if 'WinGuake' in win32gui.GetWindowText(hwnd):
+            m_width = win32api.GetSystemMetrics(0)
+            m_length = win32api.GetSystemMetrics(1)
+            w_width = int(m_width)
+            w_length = int(m_length/2)
+            win32gui.MoveWindow(hwnd, 0, 0, w_width, w_length, True)
+
+def window_resize():
+    win32gui.EnumWindows(enumHandler, None)
+
 
 def last_line(logfile):
     with open(logfile, 'rb') as fh:
@@ -15,8 +29,8 @@ def last_line(logfile):
             offs *= 2
         return last
 
-def write_to_log(path):
-    pathlog = open('path.log', 'a')
+def write_to_log(path, path_to_log):
+    pathlog = open('{}\\path.log'.format(path_to_log), 'a')
     pathlog.write('{}\n'.format(path))
     pathlog.close()
 
@@ -38,6 +52,7 @@ parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true')
 parser.add_argument
 args = parser.parse_args()
 console_running = False
+logpath = '{}\\logs'.format(os.getcwd())
 cmd = 'wmic process get description'
 proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 for line in proc.stdout:
@@ -64,7 +79,7 @@ else:
             print("WinGuake Already running!")
 
     os.system('title WinGuake')
-    os.system('python window_resize.py')
+    window_resize()
     os.system('color 0a')
     startingpath = None
     if os.path.isfile('path.log'):
@@ -76,11 +91,21 @@ else:
     os.system('cls')
     while True:
         current_dir = os.getcwd()
-        write_to_log(current_dir)
+        write_to_log(current_dir, logpath)
         command = input("{}>".format(current_dir))
         if 'cd' in command[:2]:
-            os.chdir(command[3:])
+            try:
+                os.chdir(command[3:])
+            except:
+                print("Path not found!")
+        elif len(command) is 2 and ':' in command:
+            try:
+                os.chdir("{}/".format(command))
+            except:
+                print("Drive not found!")
         elif command == 'exit':
+            os.remove("{}\\path.log".format(logpath))
+            os.system('exit')
             sys.exit()
         else:
             os.system(command)
