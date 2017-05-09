503739c183afc34f2b1b569076392cff8a27fd25
chand1012 <chandlerl14@outlook.com>
2017-05-08 22:37:02 -0400

    moved and changed files

---
+++ release_files/winguake_settings.bat
@@ -0,0 +1,4 @@
+@echo off
+cd bin
+winguake.exe --settings
+exit
---
+++ winguake_settings.bat
@@ -0,0 +1,8 @@
+@echo off
+rem for release:
+rem cd bin
+rem winguake.exe --settings
+rem exit
+
+python winguake_allinone.py --settings
+exit
