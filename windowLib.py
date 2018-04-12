41bca6a91ca3035732428454acec9da263fcbb71
Chandler Lofland <chandlerl14@outlook.com>
2018-04-12 11:29:48 -0400

    Added more things

--- windowLib.py
+++ windowLib.py
@@ -1,6 +1,7 @@
 import win32gui
 from win32api import GetSystemMetrics
-
+import psutil
+import os
 def enumHandler(hwnd, lParam):
     if win32gui.IsWindowVisible(hwnd):
         if 'WinGuake - Guake For Windows' in win32gui.GetWindowText(hwnd):
@@ -12,3 +13,19 @@ def enumHandler(hwnd, lParam):
 
 def window_resize():
     win32gui.EnumWindows(enumHandler, None)
+
+def is_running(thing):
+    return thing in (p.name() for p in psutil.process_iter())
+
+def chdir(path):
+    if '%' in path:
+        env_var = path.upper().replace('%', '')
+        #print(env_var)
+        env_path = os.getenv(env_var)
+    else:
+        env_path = path
+    #print(env_path)
+    try:
+        os.chdir(env_path)
+    except:
+        print("Path not found!")
--- winguake2.py
+++ winguake2.py
@@ -1,4 +1,46 @@
 from windowLib import *
 import sys, os
-os.system("cls")
+import argparse
+
+parser = argparse.ArgumentParser(description="Guake for Windows")
+#parser.add_argument('-s', '--settings', help="Open settings", action='store_true')
+parser.add_argument('-v', '--verbose', help='Verbose mode', action='store_true')
+#parser.add_argument('-d', '--default', help="Run with default settings", action='store_true')
+args = parser.parse_args()
+logpath = '{}\\logs'.format(os.getcwd())
+if args.verbose:
+    print("Running WinGuake in Verbose mode.")
+
+if False:
+    if args.verbose:
+        print("Running settings...")
+    os.system('python edit_settings.py')
+    #change these around when compiling
+    #os.system('winguake_settings.exe')
+else:
+    if not is_running('console.exe'):
+        if args.verbose:
+            print("WinGuake not started, starting program...")
+        run_console = os.system('console.exe')
+        if int(run_console) != 0:
+            if args.verbose:
+                print("WinGuake precompiled script not found, seeing if AutoHotkey is running...")
+            if not is_running('AutoHotkey.exe'):
+                if args.verbose:
+                    print('AutoHotkey is not running, trying to run...')
+                run_console_b  = os.system('start Console.ahk')
+                if int(run_console_b) != 0:
+                    if args.verbose:
+                        print("No console script found, skipping....")
+            else:
+                if args.verbose:
+                    print('AutoHotkey is already running!')
+    else:
+        if args.verbose:
+            print("WinGuake Already running!")
+
+
+if not args.verbose:
+    os.system("cls")
+chdir("%USERPROFILE%")
 window_resize()
