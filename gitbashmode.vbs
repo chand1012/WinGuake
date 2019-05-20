12760bb3d9d7101b75c44c3f9728258b85ac9ea7
Chandler Lofland <chandler.lofland@autobag.com>
2019-05-20 09:13:18 -0400

    minifying code

--- gitbashmode.vbs
+++ gitbashmode.vbs
@@ -1,4 +1,4 @@
 Set WshShell = CreateObject("WScript.Shell")
 WshShell.Run "git-bash", 0, True
 WScript.Sleep 1000
-WshShell.Run "python winguake2.py -m bash", 0, True ' this will need changed for the production exe
+WshShell.Run "python winguake2.py", 0, True ' this will need changed for the production exe
--- windowLib.py
+++ windowLib.py
@@ -4,17 +4,17 @@ import psutil
 import os
 
 WINDOWS = ['WinGuake - Guake For Windows', "Cmder", "MINGW32"]
-WINDOWTEST = ""
 
 def enumHandler(hwnd, lParam):
     if win32gui.IsWindowVisible(hwnd):
         #if 'WinGuake - Guake For Windows' in win32gui.GetWindowText(hwnd) or "Cmder" in win32gui.GetWindowText(hwnd) or "MINGW32" in win32gui.GetWindowText(hwnd): # porting to Cmder
-        if WINDOWTEST in win32gui.GetWindowText(hwnd):
-            m_width = GetSystemMetrics(0)
-            m_length = GetSystemMetrics(1)
-            w_width = int(m_width)
-            w_length = int(m_length/2)
-            win32gui.MoveWindow(hwnd, 0, 0, w_width, w_length, True)
+        for window in WINDOWS:
+            if window in win32gui.GetWindowText(hwnd):
+                m_width = GetSystemMetrics(0)
+                m_length = GetSystemMetrics(1)
+                w_width = int(m_width)
+                w_length = int(m_length/2)
+                win32gui.MoveWindow(hwnd, 0, 0, w_width, w_length, True)
 
 def window_resize():
     win32gui.EnumWindows(enumHandler, None)
